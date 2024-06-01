027c9c9d48058719325dc6d5c694e7574b9b87f7
Elvis Kwabena Asare Nkrumah <106641706+ElvisKwabena@users.noreply.github.com>
2024-06-01 18:05:46 +0000

    Add files via upload

---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace NkrumHub
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string response1;
+            int response2;  
+            float money;
+
+
+            Console.WriteLine("Welcome to the currency converter app.");
+            Console.WriteLine("Please enter 'c' to begin");
+
+
+            response1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter the following to convert: 1. Dollars to Ghana Cedis 2. Euros to Ghana Cedis 3. Pounds to Ghana cedis ");
+            response2=Convert.ToInt32(Console.ReadLine());  
+            
+            Console.WriteLine("Please enter your amount in your respective currencies");
+
+            money = Convert.ToSingle(Console.ReadLine());
+
+            switch (response2)
+            {
+                case 1:
+                    Console.WriteLine(money + " dollars is equivalent to "+ money * 14.74+ " Ghana Cedis");
+                        break;
+
+                case 2:
+                    Console.WriteLine(money + " euros is equivalent to " + money * 16 + " Ghana Cedis");
+                    break;
+
+                case 3:
+                    Console.WriteLine(money + " pounds is equivalent to " + money * 18.77 + " Ghana Cedis");
+                    break; 
+
+            }
+
+
+
+
+
+        }
+
+
+    }
+    
+    
+}
