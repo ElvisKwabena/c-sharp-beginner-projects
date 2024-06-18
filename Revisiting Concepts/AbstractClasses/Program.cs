62bf5217a4fefd5ceef1594db00dbfb5b00424bc
Elvis Kwabena Asare Nkrumah <106641706+ElvisKwabena@users.noreply.github.com>
2024-06-18 19:33:33 +0000

    Add files via upload

---
+++ Revisiting Concepts/AbstractClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NkrumHub
+{
+    class Program
+    {
+        abstract class Shape
+        {
+            public abstract void Draw();
+        }
+        class Circle: Shape
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("Circle Draw");
+            }
+        }
+        }
+    }
+}
