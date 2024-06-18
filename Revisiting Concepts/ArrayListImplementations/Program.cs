cfac6942593c64bab862295f77b69c4a9c05b3d6
Elvis Kwabena Asare Nkrumah <106641706+ElvisKwabena@users.noreply.github.com>
2024-06-18 19:34:13 +0000

    Add files via upload

---
+++ Revisiting Concepts/ArrayListImplementations/Program.cs
@@ -0,0 +1,26 @@
+﻿//non-generic implementation of the arrayList first
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NkrumHub
+{
+    internal class Program
+    {
+        /*ArrayList myAL = new ArrayList();
+        myAL.add(3);
+        myAL.add(4);
+        myAL.add(34);
+        
+
+
+        Console.WriteLine("Count: {0}" , myAL.Count);*/
+
+
+        List<string> ln=new List<string>();
+        ln.BinarySearch("Effah");
+        ln.Sort("Elvis");
+        ln.Remove("Kwabena");
+
+    }
+}
