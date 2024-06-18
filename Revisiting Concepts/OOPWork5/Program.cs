96cc90efe5d23ccc4f6a536b63cbea6d9e90844d
Elvis Kwabena Asare Nkrumah <106641706+ElvisKwabena@users.noreply.github.com>
2024-06-18 19:37:22 +0000

    Add files via upload

---
+++ Revisiting Concepts/OOPWork5/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Louisa
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public abstract string MakeSound();
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+
+        public void SetAge(int age)
+        {
+            Age = age;
+        }
+    }
+
+    // Lion class
+    public class Lion : Animal
+    {
+        public Lion(string name, int age) : base(name, age)
+        {
+        }
+
+        public override string MakeSound()
+        {
+            return "Roar";
+        }
+    }
+
+    // Tiger class
+    public class Tiger : Animal
+    {
+        public Tiger(string name, int age) : base(name, age)
+        {
+        }
+
+        public override string MakeSound()
+        {
+            return "Growl";
+        }
+    }
+
+    // Elephant class
+    public class Elephant : Animal
+    {
+        public Elephant(string name, int age) : base(name, age)
+        {
+        }
+
+        public override string MakeSound()
+        {
+            return "Trumpet";
+        }
+    }
+
+}
